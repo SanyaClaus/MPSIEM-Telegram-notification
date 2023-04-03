a26b211a01a81d96081922fce05261cd31fac379
SanyaClaus <s-aher@ya.ru>
2023-04-03 16:23:59 +0500

    Create settings.py
    Основные настройки выделены в отдельный файл

---
+++ settings.py
@@ -0,0 +1,19 @@
+from datetime import timedelta
+import urllib.parse  # нужно для корректной отправки паролей со спецсимволами
+
+# настройки
+pause_time = 15  # время в секундах между проверками инцидентов
+time_zone = timedelta(hours=5)
+username = "user"  # имя пользоватея в SIEM
+password = urllib.parse.quote("P@ssw0rd")  # пароль пользователя в SIEM
+client_id = "mpx"
+client_secret = "a123b456-c789-cdef-ghij-k81234567890"  # secret пользователя в SIEM
+base_url = "https://siem1.o2.local"  # url для входа в SIEM
+tg_bot_token = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # токен Телеграм-бота
+admin_chat_id = 123456789  # id чата с администратором в Телеграм
+chat_ids = [123456789, 234567890]  # id чатов в Телеграм
+default_header = {
+        "User-Agent": "python-tg-bot",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept": "*/*"
+}
